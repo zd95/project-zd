5266ab6a72186e2525c3142a864a5e14afd62c0c
DeShaoJunLin <3308953071@qq.com>
2019-10-22 20:52:27 +0800

    搭建项目的开发环境

---
+++ fullpagePro.bat
@@ -0,0 +1,5 @@
+call npm run dosass
+call npm run dopostcss
+call npm run dowebpack
+call npm run dobabel
+call npm run docopy
---
+++ util/copy.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra')
+async function copyFiles () {
+  try {
+    await fs.copy('./static', './dist/static')
+    await fs.copy('./index.html', './dist/index.html')
+    console.log('success!')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+copyFiles()
