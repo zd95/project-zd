e0ae16d8e760b2cab4cbfcb638a52815d69ef97e
DeShaoJunLin <3308953071@qq.com>
2019-10-22 19:44:49 +0800

    第一次提交项目的初始化配置文件

---
+++ src/JS/index.js
@@ -0,0 +1,9 @@
+//使用Babel编译该文件
+import "@babel/polyfill"
+import "./farme/farme"
+import "./specialtyJS/home"
+import "./specialtyJS/course"
+import "./specialtyJS/works"
+import "./specialtyJS/about"
+import "./specialtyJS/tema"
+
